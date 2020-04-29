6d6b2aa7d83dccf62f06de305f4c1b30d656c12f
Clóvis Neto <cloves13@hotmail.com>
2020-04-29 21:59:14 +0200

    Fix unit tests sample

--- test/test-data/test-blogPost-data.js
+++ test/test-data/test-blogPost-data.js
@@ -1,9 +1,9 @@
 module.exports = {
   data: [
     {
-      blogPostID: 1,
-      blogPostTitle: 'Trip Ideas',
-      blogPostSummary:
+      blogpostID: 1,
+      blogpostTitle: 'Trip Ideas',
+      blogpostSummary:
         'Atalanta B.C. fans: One of the most extreme Ultra cultures in European Football',
       imageURL:
         'https://i0.wp.com/blog.sportswhereiam.com/wp-content/uploads/2020/01/Atalanta-BC-Ultras-Fans-Bergamo-Serie-A.jpg?w=1920&ssl=1',
@@ -11,9 +11,9 @@ module.exports = {
         'https://blog.sportswhereiam.com/2020/01/atalanta-b-c-fans-one-of-the-most-extreme-ultra-cultures-in-european-football/',
     },
     {
-      blogPostID: 2,
-      blogPostTitle: 'Stadium Guide',
-      blogPostSummary:
+      blogpostID: 2,
+      blogpostTitle: 'Stadium Guide',
+      blogpostSummary:
         'Barclays Center: A visitor guide for your Brooklyn Nets or NY Islanders game',
       imageURL:
         'https://i2.wp.com/blog.sportswhereiam.com/wp-content/uploads/2019/07/Barclays-Center-Brooklyn-Nets-NY-Islanders-visitor-guide.jpg?w=1920&ssl=1',
