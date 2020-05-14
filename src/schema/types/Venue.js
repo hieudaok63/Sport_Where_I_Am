2700b479c1555a79f643dabed7f9b5c2f2ef5b47
Corey <corey.rodgers@rocketlab.com.au>
2020-05-14 15:47:25 +1000

    Add Venue Types

--- src/schema/types/Venue.js
+++ src/schema/types/Venue.js
@@ -11,15 +11,18 @@ const Venue = new GraphQLObjectType({
   name: 'Venue',
   fields: {
     photoURL: { type: GraphQLString },
-    // venueid: 440,
-    // venueWordpressID: 13798,
-    // venueName: 'Arthur Ashe Stadium',
-    // venueAddress: 'One Flushing Meadows Corona Park Road',
-    // address: 'One Flushing Meadows Corona Park Road',
-    // cityid: 9,
-    // coOrdinates: { latitude: 40.7518086, longitude: -73.85430450000001 },
-    // venueDescription:
-    events: { type: GraphqlList(Event) },
+    venueid: { type: GraphQLInt },
+    venueWordpressID: { type: GraphQLInt },
+    venueName: { type: GraphQLString },
+    venueAddress: { type: GraphQLString },
+    address: { type: GraphQLString },
+    cityid: { type: GraphQLInt },
+    coOrdinates: {
+      latitude: { type: GraphQLFloat },
+      longitude: { type: GraphQLFloat },
+    },
+    venueDescription: { type: GraphQLString },
+    events: { type: GraphQLList(Event) },
   },
 });
 
