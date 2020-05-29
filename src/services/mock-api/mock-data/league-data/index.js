b785d53399da4fa4c2be8da94685cbb10322e145
EAA <eugene.astuto@rocketlab.com.au>
2020-05-29 16:40:24 +1000

    Fix mock league data

--- src/services/mock-api/mock-data/league-data/index.js
+++ src/services/mock-api/mock-data/league-data/index.js
@@ -1,38 +1,37 @@
 module.exports = {
-  data: [
-    {
-      id: 100,
-      leagueId: 100,
-      leagueAbbreviation: 'NCAABK',
-      leagueName: 'College Basketball',
-      sport: {
-        sportID: 100,
-        sportIconURL:
-          'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_basketball.png',
-        sportIconURLV4:
-          'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_basketball.png',
-        sportName: 'Basketball',
-      },
-      leagueDescription: null,
-      photoURL: null,
-      events: null,
+  data: {
+    id: 100,
+    leagueId: 100,
+    leagueAbbreviation: 'NCAABK',
+    leagueName: 'College Basketball',
+    sport: {
+      sportID: 100,
+      sportIconURL:
+        'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_basketball.png',
+      sportIconURLV4:
+        'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_basketball.png',
+      sportName: 'Basketball',
     },
-    {
-      id: 200,
-      leagueId: 200,
-      leagueAbbreviation: 'MLB',
-      leagueName: 'Major League Baseball',
-      sport: {
-        sportID: 200,
-        sportIconURL:
-          'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_baseball.png',
-        sportIconURLV4:
-          'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_baseball.png',
-        sportName: 'Baseball',
-      },
-      leagueDescription: null,
-      photoURL: null,
-      events: null,
-    },
-  ],
+    leagueDescription: null,
+    photoURL: null,
+    events: null,
+  },
 };
+
+// {
+//   id: 200,
+//   leagueId: 200,
+//   leagueAbbreviation: 'MLB',
+//   leagueName: 'Major League Baseball',
+//   sport: {
+//     sportID: 200,
+//     sportIconURL:
+//       'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_baseball.png',
+//     sportIconURLV4:
+//       'https://api.sportswhereiam.com/swiam-api/imgs/sports/png_baseball.png',
+//     sportName: 'Baseball',
+//   },
+//   leagueDescription: null,
+//   photoURL: null,
+//   events: null,
+// },
