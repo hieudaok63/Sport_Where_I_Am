950aa5d964969e726044fb480310408e65c1ea42
nativedone <adeildoamorim@gmail.com>
2020-06-02 14:42:26 -0300

    Expose sendTicketConfirmation & getBooking queries

--- src/schema/queries/index.js
+++ src/schema/queries/index.js
@@ -11,6 +11,8 @@ import { venueByIdFromDate } from './venue';
 import { allLeagues } from './leagues';
 import { productIdByEventId } from './shop';
 import { eventById } from './event';
+import { sendTicketConfirmation } from './sendTicketConfirmation';
+import { getBooking } from './booking';
 
 export {
   cityById,
@@ -28,4 +30,6 @@ export {
   venueByIdFromDate,
   productIdByEventId,
   eventById,
+  sendTicketConfirmation,
+  getBooking,
 };
