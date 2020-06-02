66adee2ee26eaab65ff6b9e2285140258ac8d997
nativedone <adeildoamorim@gmail.com>
2020-06-02 14:40:27 -0300

    Mock findBooking & sendEmailToBookingCrew services

---
+++ src/services/booking-service.js
@@ -0,0 +1,75 @@
+const sendEmailToBookingCrew = (bookingId, email, fullName) => {
+  // TODO: Return the real API data
+  const promiseTimeout = time => () =>
+    new Promise(resolve => setTimeout(resolve, time));
+
+  return promiseTimeout(3000)()
+    .then(() => ({
+      bookingId,
+      id: email,
+      fullName,
+      email,
+      hasSent: true,
+    }))
+    .catch(error => {
+      logger.error(
+        `Error in Booking Service - sendEmailToBookingCrew(): ${email}, ${fullName}`,
+        error.message
+      );
+      console.log('login error_________', error);
+      return null;
+    });
+};
+
+export { sendEmailToBookingCrew };
+
+const findBooking = (bookingId, email, fullName) => {
+  // TODO: Return the real API data
+  const promiseTimeout = time => () =>
+    new Promise(resolve => setTimeout(resolve, time));
+
+  return promiseTimeout(3000)()
+    .then(() => ({
+      bookingId,
+      buyerFullName: fullName,
+      buyerEmail: email,
+      tickets: [
+        {
+          id: '1',
+          fullName,
+          email,
+        },
+        {
+          id: '2',
+          fullName: null,
+          email: null,
+        },
+        {
+          id: '3',
+          fullName: null,
+          email: null,
+        },
+        {
+          id: '4',
+          fullName: null,
+          email: null,
+        },
+        {
+          id: '5',
+          fullName: null,
+          email: null,
+        },
+      ],
+    }))
+    .catch(error => {
+      logger.error(
+        `Error in Booking Service - findBooking(): ${email}, ${fullName}`,
+        error.message
+      );
+      console.log('login error_________', error);
+      return null;
+    });
+};
+
+export { findBooking };
+
