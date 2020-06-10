47ec7fbf1f7681da340bb9e99168222e74e85c9c
Corey <corey.rodgers@rocketlab.com.au>
2020-06-10 12:00:10 +1000

    Update Data in Leage Teams Data

--- src/services/mock-api/mock-data/leagueTeams-data /index.js
+++ src/services/mock-api/mock-data/leagueTeams-data /index.js
@@ -60,5 +60,245 @@ module.exports = {
       teamID: 10,
       teamName: 'string',
     },
+    {
+      id: 2,
+      events: [
+        {
+          abbrev: {
+            teamA: 'string',
+            teamB: 'string',
+          },
+          buyTicketsURL: 'string',
+          cityid: 0,
+          dateTime: '2020-06-09T05:24:01.693Z',
+          dateTimeStamp: 'string',
+          eventId: 0,
+          eventName: 'string',
+          headline: 'string',
+          isFuture: true,
+          niceWhen: 'string',
+          priceFrom: 0,
+          sportIcon: 'string',
+          sportName: 'string',
+          sportid: 0,
+          teamA: 'string',
+          teamB: 'string',
+          timelineItemType: 'string',
+          venueWordpressID: 0,
+          venueid: 0,
+        },
+      ],
+      leagues: [
+        {
+          cardImageURL: 'string',
+          leagueAbbreviation: 'NBA',
+          leagueDescription: 'string',
+          leagueId: 0,
+          leagueName: 'NBA Basketball',
+          photoURL: 'string',
+          sport: {
+            sportID: 0,
+            sportIconURL:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportIconURLV4:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportName: 'Basketball',
+          },
+        },
+      ],
+      logoURL: 'string',
+      photoURL: 'string',
+      sport: {
+        sportID: 0,
+        sportIconURL:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportIconURLV4:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportName: 'Basketball',
+      },
+      teamDescription: 'Basketball Team Description',
+      teamID: 20,
+      teamName: 'string',
+    },
+    {
+      id: 3,
+      events: [
+        {
+          abbrev: {
+            teamA: 'string',
+            teamB: 'string',
+          },
+          buyTicketsURL: 'string',
+          cityid: 0,
+          dateTime: '2020-06-09T05:24:01.693Z',
+          dateTimeStamp: 'string',
+          eventId: 0,
+          eventName: 'string',
+          headline: 'string',
+          isFuture: true,
+          niceWhen: 'string',
+          priceFrom: 0,
+          sportIcon: 'string',
+          sportName: 'string',
+          sportid: 0,
+          teamA: 'string',
+          teamB: 'string',
+          timelineItemType: 'string',
+          venueWordpressID: 0,
+          venueid: 0,
+        },
+      ],
+      leagues: [
+        {
+          cardImageURL: 'string',
+          leagueAbbreviation: 'NBA',
+          leagueDescription: 'string',
+          leagueId: 0,
+          leagueName: 'NBA Basketball',
+          photoURL: 'string',
+          sport: {
+            sportID: 0,
+            sportIconURL:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportIconURLV4:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportName: 'Basketball',
+          },
+        },
+      ],
+      logoURL: 'string',
+      photoURL: 'string',
+      sport: {
+        sportID: 0,
+        sportIconURL:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportIconURLV4:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportName: 'Basketball',
+      },
+      teamDescription: 'Basketball Team Description',
+      teamID: 30,
+      teamName: 'string',
+    },
+    {
+      id: 4,
+      events: [
+        {
+          abbrev: {
+            teamA: 'string',
+            teamB: 'string',
+          },
+          buyTicketsURL: 'string',
+          cityid: 0,
+          dateTime: '2020-06-09T05:24:01.693Z',
+          dateTimeStamp: 'string',
+          eventId: 0,
+          eventName: 'string',
+          headline: 'string',
+          isFuture: true,
+          niceWhen: 'string',
+          priceFrom: 0,
+          sportIcon: 'string',
+          sportName: 'string',
+          sportid: 0,
+          teamA: 'string',
+          teamB: 'string',
+          timelineItemType: 'string',
+          venueWordpressID: 0,
+          venueid: 0,
+        },
+      ],
+      leagues: [
+        {
+          cardImageURL: 'string',
+          leagueAbbreviation: 'NBA',
+          leagueDescription: 'string',
+          leagueId: 0,
+          leagueName: 'NBA Basketball',
+          photoURL: 'string',
+          sport: {
+            sportID: 0,
+            sportIconURL:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportIconURLV4:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportName: 'Basketball',
+          },
+        },
+      ],
+      logoURL: 'string',
+      photoURL: 'string',
+      sport: {
+        sportID: 0,
+        sportIconURL:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportIconURLV4:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportName: 'Basketball',
+      },
+      teamDescription: 'General Team Description',
+      teamID: 40,
+      teamName: 'string',
+    },
+    {
+      id: 5,
+      events: [
+        {
+          abbrev: {
+            teamA: 'string',
+            teamB: 'string',
+          },
+          buyTicketsURL: 'string',
+          cityid: 0,
+          dateTime: '2020-06-09T05:24:01.693Z',
+          dateTimeStamp: 'string',
+          eventId: 0,
+          eventName: 'string',
+          headline: 'string',
+          isFuture: true,
+          niceWhen: 'string',
+          priceFrom: 0,
+          sportIcon: 'string',
+          sportName: 'string',
+          sportid: 0,
+          teamA: 'string',
+          teamB: 'string',
+          timelineItemType: 'string',
+          venueWordpressID: 0,
+          venueid: 0,
+        },
+      ],
+      leagues: [
+        {
+          cardImageURL: 'string',
+          leagueAbbreviation: 'NBA',
+          leagueDescription: 'string',
+          leagueId: 0,
+          leagueName: 'NBA Basketball',
+          photoURL: 'string',
+          sport: {
+            sportID: 0,
+            sportIconURL:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportIconURLV4:
+              'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+            sportName: 'Basketball',
+          },
+        },
+      ],
+      logoURL: 'string',
+      photoURL: 'string',
+      sport: {
+        sportID: 0,
+        sportIconURL:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportIconURLV4:
+          'https://sportswhereiam.com/wp-content/themes/swiam-white-label/images/icons/swiam/sport-basketball.png',
+        sportName: 'Basketball',
+      },
+      teamDescription: 'General Team Description',
+      teamID: 50,
+      teamName: 'string',
+    },
   ],
 };
