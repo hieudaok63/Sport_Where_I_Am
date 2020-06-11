2d323e2cd844e2fe63a86125aa20a97bf39578f2
nativedone <adeildoamorim@gmail.com>
2020-06-11 20:21:31 -0300

    Create CreditCard type and input

--- src/schema/types/shop/CreditCard.js
+++ src/schema/types/shop/CreditCard.js
@@ -1,17 +1,28 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInputObjectType,
+} from 'graphql';
+
+const fields = {
+  userId: { type: GraphQLString },
+  userName: { type: GraphQLString },
+  stripe_card_token_id: { type: GraphQLString },
+  stripe_customer_id: { type: GraphQLString },
+  credit_card_exp_year: { type: GraphQLString },
+  credit_card_exp_month: { type: GraphQLString },
+  credit_card_last_digit: { type: GraphQLString },
+  credit_card_holder_name: { type: GraphQLString },
+};
 
 const CreditCard = new GraphQLObjectType({
   name: 'CreditCard',
-  fields: {
-    userId: { type: GraphQLString },
-    userName: { type: GraphQLString },
-    stripe_card_token_id: { type: GraphQLString },
-    stripe_customer_id: { type: GraphQLString },
-    credit_card_exp_year: { type: GraphQLString },
-    credit_card_exp_month: { type: GraphQLString },
-    credit_card_last_digit: { type: GraphQLString },
-    credit_card_holder_name: { type: GraphQLString },
-  },
+  fields,
+});
+
+export const CreditCardInput = new GraphQLInputObjectType({
+  name: 'CreditCardInput',
+  fields,
 });
 
 export default CreditCard;
