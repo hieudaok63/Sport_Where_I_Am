69bb87e7cc91f5b3a61802cdb78421a7706ca006
nativedone <adeildoamorim@gmail.com>
2020-06-11 16:50:42 -0300

    Create Variant type

---
+++ src/schema/types/shop/Variant.js
@@ -0,0 +1,20 @@
+import { GraphQLObjectType, GraphQLString } from 'graphql';
+
+import PriceCart from './PriceCart';
+import Attribute from './Attribute';
+
+const Variant = new GraphQLObjectType({
+  name: 'Variant',
+  fields: {
+    id: { type: GraphQLString },
+    type: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    price: { type: PriceCart },
+    url: { type: GraphQLString },
+    urlType: { type: GraphQLString },
+    attributes: { type: Attribute },
+  },
+});
+
+export default Variant;
