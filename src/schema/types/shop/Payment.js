e73f47267147ab530250dbf7b15c1175b40d5fd8
nativedone <adeildoamorim@gmail.com>
2020-06-11 20:22:05 -0300

    Create payment input type

--- src/schema/types/shop/Payment.js
+++ src/schema/types/shop/Payment.js
@@ -1,17 +1,33 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInputObjectType,
+} from 'graphql';
 
-import PriceCart from './PriceCart';
+import PriceCart, { PriceCartInput } from './PriceCart';
+
+const fields = {
+  token: { type: GraphQLString },
+  gateway: { type: GraphQLString },
+  partner: { type: GraphQLString },
+  chargeId: { type: GraphQLString },
+  result: { type: GraphQLString },
+  refundId: { type: GraphQLString },
+};
 
 const Payment = new GraphQLObjectType({
   name: 'Payment',
   fields: {
+    ...fields,
     currencyAmount: { type: PriceCart },
-    token: { type: GraphQLString },
-    gateway: { type: GraphQLString },
-    partner: { type: GraphQLString },
-    chargeId: { type: GraphQLString },
-    result: { type: GraphQLString },
-    refundId: { type: GraphQLString },
+  },
+});
+
+export const PaymentInput = new GraphQLInputObjectType({
+  name: 'PaymentInput',
+  fields: {
+    ...fields,
+    currencyAmount: { type: PriceCartInput },
   },
 });
 
