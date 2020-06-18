d2df1e60f4d19d8949329f1d57a7685ef621bb50
EAA <eugene.astuto@rocketlab.com.au>
2020-06-18 12:49:21 +1000

    Update news and blog types due to changes in API

--- src/schema/types/NewsItem.js
+++ src/schema/types/NewsItem.js
@@ -1,10 +1,7 @@
 import {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLID,
   GraphQLInt,
-  GraphQLList,
-  GraphQLBoolean,
 } from 'graphql';
 
 import InterestTagging from './InterestTagging';
@@ -12,8 +9,8 @@ import InterestTagging from './InterestTagging';
 const NewsItem = new GraphQLObjectType({
   name: 'NewsItem',
   fields: {
-    blogpostID: { type: GraphQLInt },
-    blogpostSummary: { type: GraphQLString },
+    id: { type: GraphQLInt },
+    summary: { type: GraphQLString },
     imageAspect: { type: GraphQLString },
     imageURL: { type: GraphQLString },
     interestTagging: { type: InterestTagging },
