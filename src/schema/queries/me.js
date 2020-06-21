f62a02abadbdd50520a6b50c32bab73feec5429f
nativedone <adeildoamorim@gmail.com>
2020-06-21 16:19:44 -0300

    Create me query

---
+++ src/schema/queries/me.js
@@ -0,0 +1,15 @@
+import User from '../types/User';
+import { getMe } from '../../services/me-service';
+
+const me = {
+  type: User,
+  resolve: (rawUserData, args, req) => {
+    if (req.token) {
+      return getMe(req.token);
+    }
+    console.warn("Token is required")
+    return null;
+  },
+};
+
+export { me };
