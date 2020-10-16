46ef55be7b66bf844da8ff5f5e5130e8836bae95
Chris Saunders <tenkinetic@gmail.com>
2020-10-16 11:35:18 +1100

    SWIAM-300 : Send email when a crew member is associated with a ticket

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -4,22 +4,59 @@ const sendEmailToBookingCrew = (bookingId, email, fullName) => {
   const promiseTimeout = time => () =>
     new Promise(resolve => setTimeout(resolve, time));
 
-  return promiseTimeout(1000)()
-    .then(() => ({
-      bookingId,
-      id: email,
-      fullName,
-      email,
-      hasSent: true,
-    }))
-    .catch(error => {
-      logger.error(
-        `Error in Booking Service - sendEmailToBookingCrew(): ${email}, ${fullName}`,
-        error.message
-      );
-      console.log('login error_________', error);
-      return null;
+  return new Promise((resolve, reject) => {
+    // Authenticate with API Key
+    const createsend = require('createsend-node');
+    const auth = { apiKey: '4c6f5c9c12a93ec5b7476c089e88ffc63e849a6b4dbd8a78' };
+    const api = new createsend(auth);
+
+    // Create a details object
+    const details = {};
+
+    // Add the unique identifier for the smart email
+    details.smartEmailID = 'cbce3426-ef21-452f-98a5-72829927d4f4';
+
+    // Add the 'To' email address
+    details.to = `${fullName} <${email}>`;
+
+    // Send the smart email(and provide a callback function that takes an error and a response parameter)
+    api.transactional.sendSmartEmail(details, (err, res) => {
+      if (err) {
+        logger.error(
+          `Error in Booking Service - sendEmailToBookingCrew(): ${email}, ${fullName}`,
+          error.message
+        );
+        console.log('login error_________', error);
+        reject(error);
+      } else {
+        console.log(res);
+        resolve({
+          bookingId,
+          id: email,
+          fullName,
+          email,
+          hasSent: true,
+        });
+      }
     });
+  });
+
+  // return promiseTimeout(1000)()
+  //   .then(() => ({
+  //     bookingId,
+  //     id: email,
+  //     fullName,
+  //     email,
+  //     hasSent: true,
+  //   }))
+  //   .catch(error => {
+  //     logger.error(
+  //       `Error in Booking Service - sendEmailToBookingCrew(): ${email}, ${fullName}`,
+  //       error.message
+  //     );
+  //     console.log('login error_________', error);
+  //     return null;
+  //   });
 };
 
 export { sendEmailToBookingCrew };
