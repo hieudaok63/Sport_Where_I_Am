fe42a3428c438c0e1bc21a6d48a277c2f7b932a2
Hector Longarte <hector.longarte@rocketlab.com.au>
2021-09-13 12:09:04 +1000

    SWIAM-37: Added defensive check to avoid service failures

--- src/services/popularEvents-service.js
+++ src/services/popularEvents-service.js
@@ -3,15 +3,22 @@ import { getAuthOption } from '../tools/auth-header';
 
 const { SWIAM_OPENAPI } = process.env;
 
+// eslint-disable-next-line import/prefer-default-export
 export const getAllPopularEvents = token => {
   const url = `${SWIAM_OPENAPI}/cms/v1/popularEvents`;
-
   const http = HttpClient.getHttpClient();
   return http
     .get(url, token && getAuthOption(token))
     .then(res => {
       const result = res.data.data;
-      result.sort((a, b) => new Date(a.eventDateTime) - new Date(b.eventDateTime));
+      result.forEach(element => {
+        if (!element.eventDateTime) {
+          return result;
+        }
+      });
+      result.sort(
+        (a, b) => new Date(a.eventDateTime) - new Date(b.eventDateTime)
+      );
       return result;
     })
     .catch(error => {
