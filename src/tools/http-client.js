9993fbc3645ad5383f558f0e74a69f7ecad21212
DungNT <tuandung071099@gmail.com>
2022-03-31 11:05:19 +0700

    update timeout axios

--- src/tools/http-client.js
+++ src/tools/http-client.js
@@ -2,13 +2,13 @@ const axios = require('axios');
 
 const strTimeOut = process.env.TIME_OUT;
 // eslint-disable-next-line radix
-const intTimeOut = (strTimeOut && parseInt(process.env.TIME_OUT)) || 3000;
+const intTimeOut = 15000 || (strTimeOut && parseInt(process.env.TIME_OUT));
 
 class HttpClient {
   static getHttpClient(optionalTimeOut) {
     const timeOut = optionalTimeOut || intTimeOut;
     return axios.create({
-      timeout: timeOut,
+      timeout: intTimeOut,
     });
   }
 }
